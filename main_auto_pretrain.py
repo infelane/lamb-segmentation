73ea965401382433ca10445bf4c0a58844f058f7
lameeus <laurens.meeus@ugent.be>
2020-02-10 16:22:25 +0100

    Start experiments autoencoder pretraining for segmentation.

---
+++ main_auto_pretrain.py
@@ -0,0 +1,33 @@
+def main():
+    """
+
+    :return:
+    """
+
+    """
+    Data (all important modalities)
+    """
+
+    folder_windows = r'C:\Users\Laurens_laptop_w\OneDrive - UGent\data\10lamb'
+    x, y =
+
+    """
+    Train autoencoder
+    2) combine modalities
+    1) split modalities
+    """
+
+    """
+    Train segmentation
+    1) reuse everything
+    2) fix encoder
+    3) without pretraining
+    """
+
+
+
+    return
+
+
+if __name__ == '__main__':
+    main()
